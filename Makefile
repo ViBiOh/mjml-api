c31612446a3b64837850c7267f4fdfbac5faba08
Vincent Boutour <bob@vibioh.fr>
2019-01-05 21:15:44 +0100

    Bumping to babel7

--- Makefile
+++ Makefile
@@ -3,10 +3,12 @@ VERSION ?= $(shell git log --pretty=format:'%h' -n 1)
 AUTHOR ?= $(shell git log --pretty=format:'%an' -n 1)
 PACKAGES ?= ./...
 
+.PHONY: help
 help: Makefile
 	@sed -n 's|^##||p' $< | column -t -s ':' | sed -e 's|^| |'
 
 ## $(APP_NAME): Build app with dependencies download
+.PHONY: $(APP_NAME)
 $(APP_NAME):
 	npm ci
 	npm run build
@@ -14,15 +16,16 @@ $(APP_NAME):
 	npm i --production
 
 ## name: Output name of app
+.PHONY: name
 name:
 	@echo -n $(APP_NAME)
 
 ## version: Output sha1 of last commit
+.PHONY: version
 version:
 	@echo -n $(VERSION)
 
 ## author: Output author's name of last commit
+.PHONY: author
 author:
 	@python -c 'import sys; import urllib; sys.stdout.write(urllib.quote_plus(sys.argv[1]))' "$(AUTHOR)"
-
-.PHONY: help $(APP_NAME) name version author
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import express from 'express';
 import yargs from 'yargs';
 import server from './server';
 import app from './app';
-import monitoring from './monitoring';
 import tracing from './tracing';
-import rollbar from './rollbar';
 
 /**
  * Get options from yargs.
@@ -15,9 +13,7 @@ import rollbar from './rollbar';
 function getOptions() {
   let args = yargs.reset();
   args = server.args(args);
-  args = monitoring.args(args);
   args = tracing.args(args);
-  args = rollbar.args(args);
 
   return args.help('help').strict().argv;
 }
@@ -25,8 +21,6 @@ function getOptions() {
 const options = getOptions();
 const expressApp = express();
 
-monitoring.init(expressApp, options);
 tracing.init(expressApp, options, ['/health']);
-rollbar.init(expressApp, options);
 app.init(expressApp);
 server.init(expressApp, options);
