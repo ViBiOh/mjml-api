dbe25f82e75a6048873182f1348c124c283e818b
Vincent Boutour <bob@vibioh.fr>
2023-08-23 15:34:03 +0200

    feat(otel): Instrumenting only http
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,5 +1,5 @@
 import * as otelsdk from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 import {
@@ -15,6 +15,7 @@ const sdk = new otelsdk.NodeSDK({
       [SemanticResourceAttributes.SERVICE_NAME]: 'mjml-api',
     }),
   ),
+  instrumentations: [new HttpInstrumentation()],
   traceExporter: new OTLPTraceExporter({
     url: `http://${process.env.OTEL_ENDPOINT_URL}`,
   }),
@@ -23,7 +24,6 @@ const sdk = new otelsdk.NodeSDK({
       url: `http://${process.env.OTEL_ENDPOINT_URL}`,
     }),
   }),
-  instrumentations: [getNodeAutoInstrumentations()],
   views: new View({
     meterName: '@opentelemetry/instrumentation-http',
     attributeKeys: ['http.method', 'http.status_code'],
