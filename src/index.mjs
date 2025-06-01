90b7d43f52a622bd833aa7025a2980435f66a056
Vincent Boutour <bob@vibioh.fr>
2025-06-01 21:34:03 +0200

    ci: Try again the job approach
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- src/index.mjs
+++ src/index.mjs
@@ -3,35 +3,30 @@
 import cluster from 'cluster';
 import express from 'express';
 import helmet from 'helmet';
-import rawYargs from 'yargs';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import telemetry from './telemetry.mjs';
 import app from './app.mjs';
 import server from './server.mjs';
 
-const yargs = rawYargs();
-
-function getOptions() {
-  const args = server.args(yargs);
-
-  args.options('workerCount', {
+const options = yargs(hideBin(process.argv))
+  .env('MJML')
+  .options('workerCount', {
     required: false,
     type: 'Number',
     describe: 'Worker count for cluster mode',
     default: 1,
-  });
-
-  args.options('listenDuration', {
+  })
+  .options('listenDuration', {
     required: false,
     type: 'Number',
     describe:
       'Number of seconds during the server is started before shutting down.',
     default: 0,
-  });
+  })
+  .help('help')
+  .strict().argv;
 
-  return args.help('help').strict().argv;
-}
-
-const options = getOptions();
 const expressApp = express();
 
 expressApp.use(helmet());
